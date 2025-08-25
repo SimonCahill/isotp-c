d089208bc44213f7972fdaee383647b0120611d2
Simon Cahill <s.cahill@procyon-systems.de>
2025-08-25 11:21:49 +0200

    Fixed formatting

--- isotp_user.h
+++ isotp_user.h
@@ -12,16 +12,15 @@ void isotp_user_debug(const char* message, ...);
 
 /**
  * @brief user implemented, send can message. should return ISOTP_RET_OK when success.
- * 
+ *
  * @return may return ISOTP_RET_NOSPACE if the CAN transfer should be retried later
  * or ISOTP_RET_ERROR if transmission couldn't be completed
  */
-int  isotp_user_send_can(const uint32_t arbitration_id,
-                         const uint8_t* data, const uint8_t size
+int isotp_user_send_can(const uint32_t arbitration_id, const uint8_t* data, const uint8_t size
 #ifdef ISO_TP_USER_SEND_CAN_ARG
-                         , void* arg
+                        , void* arg
 #endif
-                        );
+);
 
 /**
  * @brief user implemented, gets the amount of time passed since the last call in microseconds
@@ -33,4 +32,3 @@ uint32_t isotp_user_get_us(void);
 #endif
 
 #endif // ISOTPC_USER_H
-
