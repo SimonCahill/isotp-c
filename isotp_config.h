56e3c12086da5f0a5097e2fd4b472ff2aaa00a29
Mark Oude Elberink <mark.oude.elberink@frickly.systems>
2025-11-06 13:33:22 +0100

    feat: make config options overridable
    Signed-off-by: Mark Oude Elberink <mark.oude.elberink@frickly.systems>

--- isotp_config.h
+++ isotp_config.h
@@ -17,22 +17,30 @@
 /* Max number of messages the receiver can receive at one time, this value
  * is affected by can driver queue length
  */
-#define ISO_TP_DEFAULT_BLOCK_SIZE 8
+#ifndef ISO_TP_DEFAULT_BLOCK_SIZE
+    #define ISO_TP_DEFAULT_BLOCK_SIZE 8
+#endif
 
 /* The STmin parameter value specifies the minimum time gap allowed between
  * the transmission of consecutive frame network protocol data units
  */
-#define ISO_TP_DEFAULT_ST_MIN_US 0
+#ifndef ISO_TP_DEFAULT_ST_MIN_US
+    #define ISO_TP_DEFAULT_ST_MIN_US 0
+#endif
 
 /* This parameter indicate how many FC N_PDU WTs can be transmitted by the
  * receiver in a row.
  */
-#define ISO_TP_MAX_WFT_NUMBER 1
+#ifndef ISO_TP_MAX_WFT_NUMBER
+    #define ISO_TP_MAX_WFT_NUMBER 1
+#endif
 
 /* Private: The default timeout to use when waiting for a response during a
  * multi-frame send or receive.
  */
-#define ISO_TP_DEFAULT_RESPONSE_TIMEOUT_US 100000
+#ifndef ISO_TP_DEFAULT_RESPONSE_TIMEOUT_US
+    #define ISO_TP_DEFAULT_RESPONSE_TIMEOUT_US 100000
+#endif
 
 /* Private: Determines if by default, padding is added to ISO-TP message frames.
  */
